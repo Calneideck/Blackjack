9315226ecbbdfdd251977dd8f8ea82fe46bcbf71
calneideck <cal_neideck@hotmail.com>
2015-10-14 10:23:56 +1100

    Added NUnit and Sample Test

---
+++ src/Tests/TestCard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Blackjack.src.Tests
+{
+    [TestFixture()]
+    class TestCard
+    {
+        [Test()]
+        public void TestCard1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
