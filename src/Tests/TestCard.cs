819a4d4748d63f134d16d778a7e88cb69bab213d
Mickito <100590093@student.swin.edu.au>
2015-10-15 13:05:28 +1100

    Fixed deal method

--- src/Tests/TestCard.cs
+++ src/Tests/TestCard.cs
@@ -58,11 +58,12 @@ namespace Blackjack.src.tests
         [Test()]
         public void TestDealer15()
         {
+
             Deck deck = new Deck();
             Dealer dealer = new Dealer();
             dealer.AddCard(new Card(Rank.KING, Suit.HEART));
             dealer.AddCard(new Card(Rank.FIVE, Suit.CLUB));
-            dealer.Deal();
+			dealer.Deal(deck);
             Assert.IsTrue(dealer.Cards.Count == 3);
         }
 
@@ -73,7 +74,7 @@ namespace Blackjack.src.tests
             Dealer dealer = new Dealer();
             dealer.AddCard(new Card(Rank.KING, Suit.HEART));
             dealer.AddCard(new Card(Rank.SIX, Suit.CLUB));
-            dealer.Deal();
+			dealer.Deal(deck);
             Assert.IsTrue(dealer.Cards.Count == 2);
         }
     }
