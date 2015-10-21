c880bfa16b2d4f4bb52e23b1427397561fb380c4
Ian Adrian <100043199@student.swin.edu.au>
2015-10-21 17:50:20 +1100

    Created Player Class and Betting Method

---
+++ src/Player.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Blackjack.src
+{
+	public class Player :Hand
+	{
+		private int _money;
+		private int _bet = 10;
+
+		public Player ()
+		{
+			_money = 100;
+			BetUp ();
+		}
+
+		public void BetUp ()
+		{
+			_money -= 10;
+			_bet += 10;
+		}
+
+		public int Money
+		{
+			get{ return _money; }
+			set{ _money = value; }
+		}
+
+		public int Bet
+		{
+			get { return _bet; }
+			set{ _bet = value; }
+		}
+
+
+		public void BetDown ()
+		{
+			if (_bet >10) 
+			{
+				_money += 10;
+				_bet -= 10;
+			}
+
+		}
+	}
+}
+
